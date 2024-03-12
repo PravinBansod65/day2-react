8309780338ef4fe5c16b863c5ef4448c66b801a3
silentSK987 <pravinbansod65@gmail.com>
2024-03-12 07:16:44 +0530

    first commint

---
+++ script.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { Component } from 'react';
+
+
+
+// ? day-2
+// const heading = React.createElement("h1", { class: "abc" }, [
+
+//   React.createElement("h2", { class: "h2" }, "Hello world"),
+//   React.createElement("h4", { class: "h4" }, "Hello h4 worsdfsfsdfsfsld"),
+// ]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <div>
+  {/* <Container/> */}
+  </div>
+); 
