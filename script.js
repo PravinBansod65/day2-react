9b8025e7db8d4889af56f735d9c012f4d1e31e2c
silentSK987 <pravinbansod65@gmail.com>
2024-03-16 21:17:35 +0530

    done

--- script.js
+++ script.js
@@ -1,39 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Component } from 'react';
+// import "./styles/App.css";
+import App from "./App.js";
 
-class Container extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { count: 0 };
-    }
 
-    increamentCount = () => {
-        const curr = this.state.count;
-        this.setState({ count: curr + 1 })
-    }
-    render() {
-        return (
-            <>
-                <p>Button clicked {this.state.count} times</p>
-                <button onClick={this.increamentCount}>Click me</button>
-            </>
-
-        )
-    }
-
-}
-
-// ? day-2
-// const heading = React.createElement("h1", { class: "abc" }, [
-
-//   React.createElement("h2", { class: "h2" }, "Hello world"),
-//   React.createElement("h4", { class: "h4" }, "Hello h4 worsdfsfsdfsfsld"),
-// ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <div>
-  <Container/> 
+  <App /> 
   </div>
 ); 
